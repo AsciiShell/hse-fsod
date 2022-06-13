2258166c74d0c3d095c1a44a0bb35be9b7b2a765
asciishell <asciishell@yandex.ru>
2022-06-13 16:24:51 +0000

    update streamlit demo

--- clip_annoy_demo/model_wrapper.py
+++ clip_annoy_demo/model_wrapper.py
@@ -1,12 +1,17 @@
+import sys
+
+sys.path.append("../notebooks")
+
 import json
 import zipfile
 
+import PIL
 import clip
 import cv2
 import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np
 import torch
-from annoy import AnnoyIndex
+from assh_utils import nms
 
 
 class Singleton(type):
@@ -27,38 +32,117 @@ def load_json(fname: str):
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-class ModelWrapper(metaclass=Singleton):
-    def __init__(self, clip_path, annoy_path, images_path):
-        self.clip_model, _ = clip.load(clip_path, device)
-        self.index = AnnoyIndex(512, "angular")
-        self.index.load(annoy_path, prefault=True)
-        self.images = pd.read_pickle(images_path)
+class Detector(metaclass=Singleton):
+    def __init__(
+            self,
+            clip_model,
+            clip_preprocess,
+            images,
+            index,
+            device,
+            cos_thr=0.2,
+            min_size=(32, 32),
+            min_conf=0.05,
+            iou_thr=0.45,
+            max_crops=10,
+    ):
+        self.clip_model = clip_model
+        self.clip_preprocess = clip_preprocess
+        self.images = images
+        self.index = index
+        self.device = device
+        self.cos_thr = cos_thr
+        self.min_size = min_size
+        self.min_conf = min_conf
+        self.iou_thr = iou_thr
+        self.max_crops = max_crops
 
-    def get_vector_text(self, x):
-        text_inputs = torch.cat([clip.tokenize(x)]).to(device)
+    def get_vector_text(self, text):
+        text_inputs = torch.cat([clip.tokenize(text)]).to(self.device)
         with torch.no_grad():
             text_features = self.clip_model.encode_text(text_inputs)
         text_features /= text_features.norm(dim=-1, keepdim=True)
         return text_features[0].cpu().numpy()
 
-    def __call__(self, text):
-        idx, dist = self.index.get_nns_by_vector(
-            self.get_vector_text(text), 9, search_k=-1, include_distances=True
-        )
+    def get_vector_image(self, x):
+        img = PIL.Image.open(x)
+        img.thumbnail((640, 640), PIL.Image.ANTIALIAS)
+        return self._get_vector_image(img)
+
+    def _get_vector_image(self, img):
+        img.show()
+        image_input = self.clip_preprocess(img).unsqueeze(0).to(self.device)
+        with torch.no_grad():
+            image_features = self.clip_model.encode_image(image_input)
+        image_features /= image_features.norm(dim=-1, keepdim=True)
+        return image_features[0].cpu().numpy()
+
+    @staticmethod
+    def _cos_sim_matrix(a, b):
+        return np.dot(a, b) / np.linalg.norm(b)
+
+    def get_ns_by_vector(self, vec, n):
+        vec = vec.astype(np.float16)
+        dist = self._cos_sim_matrix(self.index, vec)
+        idx = np.argsort(dist)
+        top_idx = idx[: -n - 1: -1]
+        return top_idx, dist[top_idx], dist
+
+    def get_ns_by_text(self, text, n):
+        return self.get_ns_by_vector(self.get_vector_text(text), n)
+
+    def get_prediction_df(self, text, n):
+        idx, dist, all_dist = self.get_ns_by_text(text, n)
         images_to_display = self.images.loc[idx].assign(dist=dist).copy()
-        fig = plt.figure(figsize=(18, 18), facecolor="w")
-        for i, (img, meta) in enumerate(images_to_display.sort_values("dist").groupby("image")):
-            # TODO fix file path
+
+        img_same = self.images[(self.images["image"].isin(images_to_display["image"]))].copy()
+        img_same["dist"] = all_dist[img_same.index]
+        img_same = img_same[
+            (
+                    (img_same["dist"] > self.cos_thr)
+                    & (img_same["xmax"] - img_same["xmin"] >= self.min_size[0])
+                    & (img_same["ymax"] - img_same["ymin"] >= self.min_size[1])
+                    & (img_same["confidence"] > self.min_conf)
+            )
+            | img_same.index.isin(images_to_display.index)
+            ]
+
+        img_nms = nms(
+            img_same[["xmin", "ymin", "xmax", "ymax"]].values,
+            img_same["confidence"].values,
+            self.iou_thr,
+            max_dets=self.max_crops,
+        )
+        img_nms = img_same.iloc[img_nms]
+        img_same["flag_orig"] = img_same.index.isin(images_to_display.index)
+        img_same["flag_nms"] = img_same.index.isin(img_nms.index)
+        return img_same
+
+    def __call__(self, text):
+        query_result = self.get_prediction_df(text, 50)
+        len_images = query_result["image"].nunique()
+        fig = plt.figure(figsize=(18, (len_images // 3 + 1) * 6), facecolor="w")
+        for i, (img, meta) in enumerate(query_result.sort_values("dist").groupby("image")):
             image = cv2.imread(img.replace("/home/jupyter/mnt", "/home/asciishell"), cv2.IMREAD_COLOR)
             assert image is not None
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             for _, row in meta.iterrows():
-                image = cv2.rectangle(image, (row["xmin"], row["ymin"]), (row["xmax"], row["ymax"]), (255, 0, 0), 3)
-            plt.subplot(3, 3, i + 1)
+                flag_orig = row['flag_orig']
+                flag_nms = row['flag_nms']
+                if not flag_nms and not flag_orig:
+                    continue
+                image = cv2.rectangle(
+                    image,
+                    (row["xmin"], row["ymin"]),
+                    (row["xmax"], row["ymax"]),
+                    (255 * flag_orig, 255 * (not flag_orig), 255 * flag_nms),
+                    2 + flag_orig * 2 + flag_nms,
+                )
+            plt.subplot(len_images // 3 + 1, 3, i + 1)
             plt.imshow(image)
-            dists = "_".join(["{:.4f}".format(x) for x in meta["dist"]])
-            confs = "_".join(["{:.4f}".format(x) for x in meta["roi_scores"]])
-            plt.title(f"Cos sim {dists}, RoI {confs}")
+            dists = "_".join(["{:.4f}".format(x) for x in meta[meta['flag_orig']]["dist"].head(5)])
+            confs = "_".join(["{:.4f}".format(x) for x in meta[meta['flag_orig']]["confidence"].head(5)])
+            plt.title(f"Cos sim {dists}, Confidence {confs}")
         plt.suptitle(f'Query: "{text}"')
         plt.tight_layout()
         return fig
--- clip_annoy_demo/streamlite_demo.py
+++ clip_annoy_demo/streamlite_demo.py
@@ -1,16 +1,18 @@
 import argparse
 import sys
 
+import clip
+import numpy as np
+import pandas as pd
 import streamlit as st
-from streamlit_drawable_canvas import st_canvas
 
-from model_wrapper import ModelWrapper
+from model_wrapper import Detector, device
 
 
 def parse_args(args):
     parser = argparse.ArgumentParser('Model config files')
     parser.add_argument('--clip', help='CLIP model path', required=True)
-    parser.add_argument('--annoy', help='Annoy index path', required=True)
+    parser.add_argument('--embeddings', help='Embeddings index path', required=True)
     parser.add_argument('--images', help='Images path', required=True)
     return parser.parse_args(args)
 
@@ -20,8 +22,23 @@ args = parse_args(sys.argv[1:])
 
 @st.experimental_singleton
 def create_model():
-    model = ModelWrapper(args.clip, args.annoy, args.images)
-    return model
+    images = pd.read_pickle(args.images)
+    clip_model, clip_preprocess = clip.load(args.clip, device)
+    index = np.load(args.embeddings)
+
+    detector = Detector(
+        clip_model,
+        clip_preprocess,
+        images,
+        index,
+        device,
+        cos_thr=0.2,
+        min_size=(32, 32),
+        min_conf=0.05,
+        iou_thr=0.45,
+        max_crops=10,
+    )
+    return detector
 
 
 def main():
@@ -30,47 +47,17 @@ def main():
     if "color_to_label" not in st.session_state:
         st.session_state["color_to_label"] = {}
     pages = {
-        "Annoy demo": annoy_demo,
-        "Keypoints demo": keypoints_demo,
+        "Query demo": query_demo,
+        # "Keypoints demo": keypoints_demo,
     }
     page = st.sidebar.selectbox("Page:", options=list(pages.keys()))
     pages[page]()
 
 
-def annoy_demo():
+def query_demo():
     model = create_model()
     x = st.sidebar.text_input("Query text", key="name")
     st.write(model(x))
-
-
-def keypoints_demo():
-    drawing_mode = 'point'
-    stroke_width = 3
-    point_display_radius = 3
-    stroke_color = "#000"
-    bg_color = "#eee"
-    # bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
-    realtime_update = True
-    height = 500
-
-    # Create a canvas component
-    canvas_results = []
-    for i in range(3):
-        canvas_result = st_canvas(
-            fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-            stroke_width=stroke_width,
-            stroke_color=stroke_color,
-            background_color=bg_color,
-            background_image=None,
-            update_streamlit=realtime_update,
-            height=height,
-            drawing_mode=drawing_mode,
-            point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
-            display_toolbar=True,
-            key=f"full_app_{i}",
-        )
-
-        canvas_results.append(canvas_result)
 
 
 if __name__ == "__main__":
